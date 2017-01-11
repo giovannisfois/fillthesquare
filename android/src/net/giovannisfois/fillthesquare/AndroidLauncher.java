b81d81894ca26701e238c524023559b19151172c
giovans <giovans@tiscali.it>
2017-01-11 10:07:51 +0100

    Created an empty libgdx project

---
+++ android/src/net/giovannisfois/fillthesquare/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.giovannisfois.fillthesquare;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.giovannisfois.fillthesquare.FillTheSquare;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FillTheSquare(), config);
+	}
+}
